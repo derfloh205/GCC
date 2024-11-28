32b79dc987d5d99eab252e040eaabcad26c5180b
Genju <derfloh205@gmail.com>
2024-11-28 06:55:40 +0100

    gitpull

--- gitpull.lua
+++ gitpull.lua
@@ -1,14 +1,19 @@
 local baseUrl = "https://raw.githubusercontent.com/derfloh205/GTurtle/refs/heads/main/"
 local files = {
-    "ckassics", "gitpull", "gnav", "init", "pull", "table_utils", "vector_utils"
+    "classics", "gitpull", "gnav", "init", "pull", "table_utils", "vector_utils"
 }
 
-for _, f in files do
+for _, f in ipairs(files) do
     local fileName = f .. ".lua"
     local url = baseUrl .. fileName
     local response = http.get(url)
-    local fileContent = response.readAll()
-    local file = fs.open(fileName, "w")
-    file.write(fileContent)
-    file.close()
+    if response then
+        print("Pulling " .. fileName .. " ..")
+        local fileContent = response.readAll()
+        local file = fs.open(fileName, "w")
+        file.write(fileContent)
+        file.close()
+    else
+        print("Failed to Pull: " .. fileName)
+    end
 end
