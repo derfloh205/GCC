cdb5ecd30ac2655f76c642904394814225a628ac
Genju <derfloh205@gmail.com>
2024-11-28 14:04:14 +0100

    gitpull variant

--- gitpull.lua
+++ gitpull.lua
@@ -12,18 +12,25 @@ local files = {
 for _, f in ipairs(files) do
     local fileName = f .. ".lua"
     local url = baseUrl .. fileName
-    local response = http.get(url)
-    if response then
-        print("Pulling " .. fileName .. " ..")
-        local filePath = fs.combine(baseDir, fileName)
-        if fs.exists(filePath) then
-            fs.delete(filePath)
-        end
-        local fileContent = response.readAll()
-        local file = fs.open(filePath, "w")
-        file.write(fileContent)
-        file.close()
-    else
-        print("Failed to Pull: " .. fileName)
-    end
+    shell.run("rm", fileName)
+    shell.run("wget", url, fileName)
 end
+
+-- for _, f in ipairs(files) do
+--     local fileName = f .. ".lua"
+--     local url = baseUrl .. fileName
+--     local response = http.get(url)
+--     if response then
+--         print("Pulling " .. fileName .. " ..")
+--         local filePath = fs.combine(baseDir, fileName)
+--         if fs.exists(filePath) then
+--             fs.delete(filePath)
+--         end
+--         local fileContent = response.readAll()
+--         local file = fs.open(filePath, "w")
+--         file.write(fileContent)
+--         file.close()
+--     else
+--         print("Failed to Pull: " .. fileName)
+--     end
+-- end
