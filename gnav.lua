ff6b48110c65099a4141baba3bd938d39f3cdc13
Genju <derfloh205@gmail.com>
2024-11-29 13:13:41 +0100

    isTurtlePosDebug

--- gnav.lua
+++ gnav.lua
@@ -75,6 +75,9 @@ end
 
 ---@return boolean isTurtlePos
 function GNAV.GridNode:IsTurtlePos()
+    self.gridMap.gridNav.gTurtle:Log(
+        "IsTurtlePos? " .. tostring(self.pos) .. " / " .. tostring(self.gridMap.gridNav.pos)
+    )
     return self.pos == self.gridMap.gridNav.pos
 end
 
