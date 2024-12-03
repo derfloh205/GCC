8442108def9fe097ed2aa8fad9e5148e7d720df8
Genju <derfloh205@gmail.com>
2024-12-03 06:17:13 +0100

    gpull

--- Util/gpull.lua
+++ Util/gpull.lua
@@ -12,15 +12,22 @@ local user = args[1]
 local repo = args[2]
 local token = args[3]
 
+local headers = {
+    ["Accept"] = "application/vnd.github+json",
+    ["Authorization"] = string.format("Bearer %s", token),
+    ["X-GitHub-Api-Version"] = "2022-11-28"
+}
+
 function GPull:Get(url)
     -- TODO: Use Token for auth
+    return http.get(url, headers)
 end
 
 function GPull:UpdateBlob(commitSha, path, fileName)
     print("Pulling " .. fileName .. " ..")
     local fileUrl =
         string.format("https://raw.githubusercontent.com/%s/%s/%s/%s/%s", user, repo, commitSha, path, fileName)
-    local response = http.get(fileUrl)
+    local response = self:Get(fileUrl)
     if response then
         local basePath = fs.combine(repo, path)
         fs.makeDir(basePath)
@@ -38,7 +45,7 @@ function GPull:UpdateTree(commitSha, path, treeData)
     local sha = treeData.sha
     -- compare to cache, only update if different
     local url = treeData.url
-    local treeResponse = http.get(url)
+    local treeResponse = self:Get(url)
     local treeJson = textutils.unserialiseJSON(treeResponse.readAll())
     local subTreeList = treeJson.tree
 
@@ -54,7 +61,7 @@ end
 function GPull:PullRepository()
     print(string.format("Pulling from github.com/%s/%s", user, repo))
     local commitApiUrl = string.format("https://api.github.com/repos/%s/%s/commits", user, repo)
-    local commitResponse = http.get(commitApiUrl)
+    local commitResponse = self:Get(commitApiUrl)
     local commitResponseJSON = textutils.unserialiseJSON(commitResponse.readAll())
     local commitSha = commitResponseJSON[1].sha
 
