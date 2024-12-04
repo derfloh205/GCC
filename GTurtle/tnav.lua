f89b683e05d2153b97dac91ec901d526ddc5e8af
Genju <derfloh205@gmail.com>
2024-12-04 12:02:17 +0100

    d

--- GTurtle/tnav.lua
+++ GTurtle/tnav.lua
@@ -517,7 +517,7 @@ function TNAV.GridNav:GetNextMoveAlongPath()
                 if requiredHead == TNAV.MOVE.U or requiredHead == TNAV.MOVE.D then
                     return requiredHead
                 end
-                self.gTurtle:Log(f("(%s) -> (%s) / (%s)", self.pos, nextPos, vecDiff))
+                self.gTurtle:Log(f("(%s) -> (%s) / (%s)", tostring(self.pos), tostring(nextPos), tostring(vecDiff)))
                 self.gTurtle:Log(f("H: %s | R: %s", self.head, requiredHead))
                 -- determine available move to reach the next position based on current heading and required heading
                 -- e.g. if I need to move nord, and I am looking south, I can also just move back
