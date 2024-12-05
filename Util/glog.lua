372697c0aaad4ab2aac055449316e7626a513e9f
Genju <derfloh205@gmail.com>
2024-12-05 07:56:41 +0100

    flog

--- Util/glog.lua
+++ Util/glog.lua
@@ -37,15 +37,40 @@ function GLogAble:SetLogFile(logFile)
     self.logFile = logFile or f("PC_%d.log", os.getComputerID())
 end
 
----@param logValue string | table
-function GLogAble:Log(logValue)
+---@param logString string?
+function GLogAble:WriteLogFile(logString)
+    local logFile = fs.open(self.logFile, "a")
+    logFile.write(f("[%s]: %s\n", os.date("%T"), logString or ""))
+    logFile.close()
+end
+
+---@param logString string | table
+function GLogAble:Log(logString)
     if not self.log then
         return
     end
-    local logString = logValue
-    if type(logValue) == "table" then
-        logString = f("\n%s", tostring(textutils.serialise(logValue)))
+
+    self:WriteLogFile(tostring(logString))
+end
+
+---@param fString string
+---@param ... any
+---@return string
+function GLogAble:FLog(fString, ...)
+    local fArgs = ...
+    local strings = {}
+    for _, arg in fArgs do
+        table.insert(strings, tostring(arg))
     end
+
+    return f(fString, table.unpack(strings))
+end
+
+function GLogAble:LogTable(t)
+    if not self.log then
+        return
+    end
+    local logString = f("\n%s", tostring(textutils.serialise(t)))
 
     local logFile = fs.open(self.logFile, "a")
     logFile.write(f("[%s]: %s\n", os.date("%T"), logString))
