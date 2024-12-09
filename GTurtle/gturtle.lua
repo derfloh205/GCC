4bb3da6241fae27b78522401aefdbe1f3f5b21ce
Genju <derfloh205@gmail.com>
2024-12-09 12:52:29 +0100

    refuel again

--- GTurtle/gturtle.lua
+++ GTurtle/gturtle.lua
@@ -57,6 +57,8 @@ function GTurtle.Base:new(options)
     self.term = options.term or term
     options.avoidUnknown = options.avoidUnknown or false
 
+    self:Refuel()
+
     if term ~= self.term then
         term:redirect(self.term)
     end
