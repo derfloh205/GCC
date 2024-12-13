0de125eb1464509d1d7836cc450a9fb618b004ce
Genju <derfloh205@gmail.com>
2024-12-13 11:38:49 +0100

    Simplify message sent by Discord webhook in hooktest.lua

--- Run/scripts/hooktest.lua
+++ Run/scripts/hooktest.lua
@@ -5,4 +5,4 @@ local hook =
     "https://discord.com/api/webhooks/1317077071863742504/3z9krO8cJfi778veJtniXPBEQ1-8gPJAxpsekzuZTNBWC7_tY4bijIyiXINCqCWO-QuU"
 )
 
-hook:sendMessage("Hello!\n-- Send from my Pocket Phone")
+hook:sendMessage("Hello!")
