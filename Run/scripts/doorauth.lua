b496263e53d718e198f758ee0846104755b7bdf4
Genju <derfloh205@gmail.com>
2024-12-21 12:56:40 +0100

    Add logging options to DoorAuthHost initialization

--- Run/scripts/doorauth.lua
+++ Run/scripts/doorauth.lua
@@ -1,4 +1,8 @@
 local DoorAuthHost = require("GCC/GAuth/DoorAuth/doorauthhost")
 local monitor = peripheral.find("monitor")
 term.redirect(monitor)
-DoorAuthHost {}:Run()
+DoorAuthHost {
+    logFile = "doorauthhost.log",
+    log = true,
+    clearLog = true
+}:Run()
